a5abfaf0289479f703307409938b4bf36179bb59
김가빈 <qwer65305621@gmail.com>
2026-07-13 22:54:01 +0900

    [level 0] Title: 코드 처리하기, Time: 7.26 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,25 +1,16 @@
 def solution(code):
-    mode=0
-    ret=''
-    i=0
-    for x in code:
-        if x=='1':
-            mode=change(mode)
+    mode = 0
+    ret = ''
+    for i, x in enumerate(code):
+        if x == '1':
+            mode = 1 - mode
         else:
-            if mode==0:
-                if i%2==0:
-                    ret+=code[i]
-            elif mode==1:
-                if i%2==1:
-                    ret+=code[i]
-        i+=1
-    if ret=='':
+            if mode == 0:
+                if i % 2 == 0:
+                    ret += x
+            elif mode == 1:
+                if i % 2 == 1:
+                    ret += x
+    if ret == '':
         return 'EMPTY'
     return ret
-            
-def change(mode):
-    if mode==0:
-        mode=1
-    elif mode==1:
-        mode=0
-    return mode
