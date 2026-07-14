cb093c107d312185f8d257e8c0d2eb51fdb82550
김가빈 <qwer65305621@gmail.com>
2026-07-14 14:49:04 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,8 +1,3 @@
-def solution(num_list):
-    i=len(num_list)
-    if num_list[i-1]>num_list[i-2]:
-        num_list.append(num_list[i-1]-num_list[i-2])
-    else:
-        num_list.append(num_list[i-1]*2)
-    
-    return num_list
+def solution(l):
+    l.append(l[-1]-l[-2] if l[-1]>l[-2] else l[-1]*2)
+    return l
