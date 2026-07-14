7883f019dd3fe15aa91871e36fffa088ce76e51a
김가빈 <qwer65305621@gmail.com>
2026-07-14 15:11:35 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.20 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,11 +1,3 @@
 def solution(n, c):
-    for x in c:
-        if x=='w':
-            n+=1
-        elif x=='s':
-            n-=1
-        elif x=='d':
-            n+=10
-        elif x=='a':
-            n-=10
-    return n
+    key=dict(zip(['w','s','d','a'], [1, -1, 10, -10]))
+    return n+sum([key[c] for c in c])
