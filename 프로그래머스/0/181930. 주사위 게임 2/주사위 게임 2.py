81bb8a00255dca0dea17f8b21f37691dfda3174e
김가빈 <qwer65305621@gmail.com>
2026-07-14 11:25:59 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,8 +1,9 @@
 def solution(a, b, c):
-    if a!= b and b!=c and a!=c:
+    x=len(set([a,b,c]))
+    if x==3:
         return a+b+c
-    elif a==b==c:
+    elif x==1:
         return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
-    else:
+    elif x==2:
         return (a+b+c)*(a**2+b**2+c**2)
     
