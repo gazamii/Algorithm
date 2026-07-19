506021e8cf9dc223d8a96a39a9f0328d48654b90
김가빈 <qwer65305621@gmail.com>
2026-07-19 16:29:45 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,15 +1,2 @@
 def solution(x1, x2, x3, x4):
-    a=Set(x1, x2)
-    b=Set(x3, x4)
-    
-    return True if a==b==True else False
-
-def Set(x1, x2):
-    set1=set([x1, x2])
-    if len(set1)==1:
-        if x1==True:
-            return True
-        else:
-            return False
-    else:
-        return True
+    return (x1 | x2) & (x3 | x4)
