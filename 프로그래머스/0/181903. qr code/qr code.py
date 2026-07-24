8e52e6852ed2824c1a7be3bbdaf025b97ad31936
김가빈 <qwer65305621@gmail.com>
2026-07-24 13:49:36 +0900

    [level 0] Title: qr code, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.py
+++ 프로그래머스/0/181903. qr code/qr code.py
@@ -1,7 +1,3 @@
 def solution(q, r, code):
-    answer = []
-    for x in range(0, len(code)):
-        if x%q==r:
-            answer.append(code[x])
-    return "".join(answer)
+    return code[r::q]
     
