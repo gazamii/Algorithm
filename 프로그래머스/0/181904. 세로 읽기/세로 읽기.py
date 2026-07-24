5b9157e7cbe6408d1ddfbe23b47d001e083306aa
김가빈 <qwer65305621@gmail.com>
2026-07-24 13:42:57 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,8 +1,5 @@
-def solution(my_string, m, c):
-    result=[]
-    my_string=list(my_string)
-    for x in range(c-1, len(my_string), m):
-        result.append(my_string[x])
-    return "".join(result)
+def solution(s, m, c):
+    return s[c-1::m]
+
     
     
