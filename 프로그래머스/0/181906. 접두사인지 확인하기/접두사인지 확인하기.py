50a2ee79978f7d455303a9b84c96e5f9068da35b
김가빈 <qwer65305621@gmail.com>
2026-07-24 13:05:27 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,5 +1,2 @@
 def solution(my_string, is_prefix):
-    liist=[]
-    for n in range(1, len(my_string)+1):
-        liist.append(my_string[:n])
-    return 1 if is_prefix in liist else 0
+    return int(my_string.startswith(is_prefix))
